ba713070c279e8245ac5d80cea39ed4a26203c24
Taniguchi,Hiroki <C0021@senju.com>
2017-11-09 16:52:53 +0900

    初期Version

---
+++ Tasks/ReplaceModule.ps1
@@ -0,0 +1,91 @@
+#
+# Function
+#
+
+function ReplaceInXmlDoc(){
+  Param(
+    [Parameter(Mandatory=$True)]
+    $XmlDoc,
+    [Parameter(Mandatory=$True)]
+    $XPath,
+    [Parameter(Mandatory=$False)]
+    $Replacement,
+    [Parameter(Mandatory=$True)]
+    [hashtable]$nameSpaces
+  )
+
+  Write-Host "--- NameSpaces:"
+  Write-Host ($NameSpaces | Out-String -Width 256)
+  Write-Host "--- XPath: $XPath"
+
+  $cloneDoc = $xmlDoc.Clone()
+
+  if ($nameSpaces.count -gt 0) {
+    $nodes = $cloneDoc | Select-Xml $XPath -Namespace $nameSpaces
+  }
+  else {
+    $nodes = $cloneDoc | Select-Xml $XPath
+  }
+  Write-Host found: $nodes.count
+  if($nodes.count -gt 0 ){
+    $nodes | ReplaceNodeValue -Replacement $Replacement
+  }
+
+  $cloneDoc
+}
+
+
+function ReplaceInFile() {
+  param(
+    [Parameter(Mandatory=$True)]
+    [string]$file,
+    [Parameter(Mandatory=$True)]
+    [string]$XPath,
+    [Parameter(Mandatory=$False)]
+    [string]$Replacement,
+    [Parameter(Mandatory=$True)]
+    [hashtable]$nameSpaces
+  )
+  Write-Host "`n--- Processing file: $file"
+#Write-Host "--- NameSpaces:"
+#$nameSpaces.Keys | %{Write-Host $_ "=" $nameSpaces[$_]}
+#Write-Host "--- XPath: $XPath"
+
+  $xmlDoc = new-object System.Xml.XmlDocument
+  $xmlDoc.Load($file)
+
+  $ReplaceDoc = ReplaceInXmlDoc $xmlDoc $XPath $Replacement $nameSpaces
+  $ReplaceDoc.Save($file)
+
+}
+
+function PrintNodes(){
+  param(
+    [parameter(ValueFromPipeline=$true)]
+    [System.Xml.XmlNode]$node,
+    [string]$prefix = ""
+  )
+  process
+  {
+    Write-Host $prefix $nodes.GetType().FullName
+    Write-Host $prefix "Type:"$node.NodeType "  Name:"  $node.name "  value:" $node.value "  text:"$node.innerText
+    if($node.hasChildNodes){
+      $node.ChildNodes | PrintNodes -prefix "  $prefix"
+    }
+  }
+}
+
+function ReplaceNodeValue(){
+  param(
+      [parameter(ValueFromPipeline = $true)]
+      $node,
+      $Replacement
+  )
+  process
+  {
+    if(!$node){return}
+    Write-Host $node.Node.value => $Replacement
+    $node.Node.value = $Replacement
+  }
+}
+
---
+++ Tasks/task.ps1
@@ -0,0 +1,47 @@
+﻿#
+# Script replaces content in files based on xpath expression
+#
+
+[CmdletBinding()]
+Param(
+	#for example, '**/*.vsixmanifest'
+	[Parameter(Mandatory=$True)]
+	[string] $FileMask,
+	
+	#if wants attribute replace, ex)'//def:PackageManifest/def:Metadata/def:Identity/@Version'
+	#if wants element text replace, ex)'//def:PackageManifest/def:Metadata/def:Description/text()'
+	[Parameter(Mandatory=$True)]
+	[string] $XPath,
+	
+	#for example, '1.5.2.2'
+	[Parameter(Mandatory=$False)]
+	[string] $Replacement,
+
+	#@{
+	#	def="http://schemas.microsoft.com/developer/vsx-schema/2011"
+  # d="http://schemas.microsoft.com/developer/vsx-schema-design/2011"
+	#}
+	[Parameter(Mandatory=$False)]
+	[string]$Namespaces
+)
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+. $here\ReplaceModule.ps1
+
+#
+# Constants
+#
+$errorActionPreference = "Stop"
+
+
+#
+# Main programme
+#
+
+gci -Recurse $FileMask | %{
+	if (! $_.PSIsContainer) {
+      [hashtable]$Hash = $Namespaces | ConvertFrom-StringData
+      ReplaceInFile $_.FullName $XPath $Replacement $Hash
+	}
+}
+
